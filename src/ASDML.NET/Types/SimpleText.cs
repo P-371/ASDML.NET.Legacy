1d4dff4510c37ea0b917c39d808fc0176c824004
P-371 <p371.hilmortz@gmail.com>
2020-07-15 15:48:24 +0200

    Add simple text literal checking

--- src/ASDML.NET/Types/SimpleText.cs
+++ src/ASDML.NET/Types/SimpleText.cs
@@ -1,10 +1,15 @@
+using System;
+
 namespace P371.ASDML.Types
 {
     public class SimpleText : Text
     {
         internal SimpleText(string value) : base(value: value) { }
 
-        public static implicit operator SimpleText(string value) => new SimpleText(value: value);
+        public static implicit operator SimpleText(string value)
+            => IsSimple(text: value)
+            ? new SimpleText(value: value)
+            : throw new ArgumentException(message: "The value is not a simple text literal", paramName: nameof(value));
 
         public static implicit operator string(SimpleText text) => text.Value;
     }
--- src/ASDML.NET/Types/Text.cs
+++ src/ASDML.NET/Types/Text.cs
@@ -4,7 +4,23 @@ namespace P371.ASDML.Types
     {
         internal Text(string value) : base(value: value) { }
 
-        public static implicit operator Text(string value) => new Text(value: value);
+        public static bool IsSimple(string text)
+        {
+            if (text.Length == 0 || (!char.IsLetter(c: text[index: 0]) && text[index: 0] != '_'))
+            {
+                return false;
+            }
+            for (int i = 1; i < text.Length; i++)
+            {
+                if (!char.IsLetterOrDigit(c: text[index: i]) && !text[index: i].In('_', '.'))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static implicit operator Text(string value) => IsSimple(text: value) ? new SimpleText(value: value) : new Text(value: value);
 
         public static implicit operator string(Text text) => text.Value;
     }

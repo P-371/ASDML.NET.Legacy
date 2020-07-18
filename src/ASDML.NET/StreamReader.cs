3323a6c03b017b502c314f65c71a8348c45d56bc
P-371 <p371.hilmortz@gmail.com>
2020-07-18 16:41:30 +0200

    Add basic StreamReader

---
+++ src/ASDML.NET/StreamReader.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Text;
+using System.IO;
+using IOStreamReader = System.IO.StreamReader;
+using P371.ASDML.Exceptions;
+using P371.ASDML.Types;
+
+namespace P371.ASDML
+{
+    internal class StreamReader : IDisposable
+    {
+        private TextReader reader { get; }
+
+        public bool EndOfStream => Peek() == -1;
+
+        public int Line { get; private set; } = 1;
+
+        public int Column { get; private set; } = 0;
+
+        public StreamReader(Stream stream)
+        {
+            reader = new IOStreamReader(stream: stream);
+        }
+
+        public StreamReader(Stream stream, Encoding encoding)
+        {
+            reader = new IOStreamReader(stream: stream, encoding: encoding);
+        }
+
+        public StreamReader(TextReader reader)
+        {
+            this.reader = reader;
+        }
+
+        public char Read()
+        {
+            char read = (char)reader.Read();
+            if (read == '\n')
+            {
+                Column = 0;
+                Line++;
+            }
+            else
+            {
+                Column++;
+            }
+            return read;
+        }
+
+        public (string text, char hit) ReadUntil(Func<char, bool> continueReading)
+        {
+            if (continueReading == null)
+            {
+                throw new ArgumentNullException(paramName: nameof(continueReading));
+            }
+            StringBuilder builder = new StringBuilder();
+            while (true)
+            {
+                char peek = Peek();
+                if (peek == -1 || !continueReading(arg: peek))
+                {
+                    return (text: builder.ToString(), hit: peek);
+                }
+                builder.Append(value: Read());
+            }
+        }
+
+        public char Peek() => Peek();
+
+        public void Dispose() => reader.Dispose();
+    }
+}

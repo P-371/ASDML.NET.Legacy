453388e65f0b3d9f0f8adb1d649580e1210f0caa
P-371 <p371.hilmortz@gmail.com>
2020-07-21 03:06:51 +0200

    Add a bunch of operators to Number

--- src/ASDML.NET/ASDML.NET/Types/Number.cs
+++ src/ASDML.NET/ASDML.NET/Types/Number.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace P371.ASDML.Types
 {
     public class Number : Object<double>
@@ -7,6 +9,23 @@ namespace P371.ASDML.Types
         internal Number(double value) : this(value, value.ToString()) { }
 
         internal Number(double value, string rawValue) : base(value) => RawValue = rawValue;
+
+        // https://stackoverflow.com/questions/1130698/checking-if-an-object-is-a-number-in-c-sharp
+        private static bool IsNumeric(object obj)
+            => obj is sbyte
+            || obj is byte
+            || obj is short
+            || obj is ushort
+            || obj is int
+            || obj is uint
+            || obj is long
+            || obj is ulong
+            || obj is float
+            || obj is double
+            || obj is decimal;
+
+        public override bool Equals(object obj)
+            => IsNumeric(obj) ? Convert.ToDouble(obj) == Value : base.Equals(obj);
 
         public static implicit operator Number(string rawValue)
             => double.TryParse(rawValue, out double result)
@@ -18,5 +37,21 @@ namespace P371.ASDML.Types
         public static implicit operator Number(double value) => new Number(value);
 
         public static implicit operator double(Number number) => number.Value;
+
+        public static bool operator <(Number left, Number right) => left.Value < right.Value;
+        public static bool operator <(Number left, double right) => left.Value < right;
+        public static bool operator <(double left, Number right) => left < right.Value;
+
+        public static bool operator >(Number left, Number right) => left.Value > right.Value;
+        public static bool operator >(Number left, double right) => left.Value > right;
+        public static bool operator >(double left, Number right) => left > right.Value;
+
+        public static bool operator <=(Number left, Number right) => left.Value <= right.Value;
+        public static bool operator <=(Number left, double right) => left.Value <= right;
+        public static bool operator <=(double left, Number right) => left <= right.Value;
+
+        public static bool operator >=(Number left, Number right) => left.Value >= right.Value;
+        public static bool operator >=(Number left, double right) => left.Value >= right;
+        public static bool operator >=(double left, Number right) => left >= right.Value;
     }
 }

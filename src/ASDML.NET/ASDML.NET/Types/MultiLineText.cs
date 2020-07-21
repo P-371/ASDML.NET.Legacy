59bc7f4f5dfb4ee36c0814c49a5f66c1e9c61051
P-371 <p371.hilmortz@gmail.com>
2020-07-21 21:13:11 +0200

    Add Text documentation

--- src/ASDML.NET/ASDML.NET/Types/MultiLineText.cs
+++ src/ASDML.NET/ASDML.NET/Types/MultiLineText.cs
@@ -2,15 +2,28 @@ using System;
 
 namespace P371.ASDML.Types
 {
-    public class MultiLineText : Text
+    /// <summary>
+    /// A Multiline Text in ASDML
+    /// </summary>
+    public sealed class MultiLineText : Text
     {
         internal MultiLineText(string value) : base(value) { }
 
+        /// <summary>
+        /// Creates an ASDML Multiline Text Literal from a <see cref="string" />.
+        /// This will check if the given text is Multiline Text Literal and throws an <see cref="ArgumentException" /> if not
+        /// </summary>
+        /// <exception cref="ArgumentException">If <paramref name="value" /> is not a Multiline Text Literal</exception>
+        /// <param name="value">The <see cref="string" /> to convert</param>
         public static implicit operator MultiLineText(string value)
             => IsMultiline(value)
             ? new MultiLineText(value)
             : throw new ArgumentException("The value is not a multiline text literal", nameof(value));
 
+        /// <summary>
+        /// Converts an ASDML Multiline Text Literal to a <see cref="string" />
+        /// </summary>
+        /// <param name="text">The <see cref="MultiLineText" /> to convert</param>
         public static implicit operator string(MultiLineText text) => text.Value;
     }
 }
--- src/ASDML.NET/ASDML.NET/Types/Text.cs
+++ src/ASDML.NET/ASDML.NET/Types/Text.cs
@@ -1,9 +1,17 @@
 namespace P371.ASDML.Types
 {
+    /// <summary>
+    /// A Text in ASDML
+    /// </summary>
     public class Text : Object<string>
     {
         internal Text(string value) : base(value) { }
 
+        /// <summary>
+        /// Checks if the given <see cref="string" /> is a Simple Text Literal
+        /// </summary>
+        /// <param name="text">The text to check</param>
+        /// <returns>The given text is a Simple Text Literal or not</returns>
         public static bool IsSimple(string text)
         {
             char[] disallowed = { '"', '(', ')', '[', ']', '{', '}' };
@@ -22,11 +30,25 @@ namespace P371.ASDML.Types
             return true;
         }
 
+        /// <summary>
+        /// Checks if the given <see cref="string" /> is a Multiline Text Literal
+        /// </summary>
+        /// <param name="text">The text to check</param>
+        /// <returns>The given text is a Multiline Text Literal or not</returns>
         public static bool IsMultiline(string text) => text.Contains("\n");
 
+        /// <summary>
+        /// Creates an ASDML Text Literal from a <see cref="string" />.
+        /// This will check if the given text is Simple Text Literal or Multiline Text Literal and return the correct type
+        /// </summary>
+        /// <param name="value">The <see cref="string" /> to convert</param>
         public static implicit operator Text(string value)
             => IsSimple(value) ? new SimpleText(value) : IsMultiline(value) ? new MultiLineText(value) : new Text(value);
 
+        /// <summary>
+        /// Converts an ASDML Text Literal to a <see cref="string" />
+        /// </summary>
+        /// <param name="text">The <see cref="Text" /> to convert</param>
         public static implicit operator string(Text text) => text.Value;
     }
 }

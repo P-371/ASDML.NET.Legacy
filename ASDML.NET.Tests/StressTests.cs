a97bbb3deff75ca0df1a26157da95c0a34dbafd7
P-371 <p371.hilmortz@gmail.com>
2020-08-12 18:27:41 +0200

    Add parser test with random long data

---
+++ ASDML.NET.Tests/StressTests.cs
@@ -0,0 +1,54 @@
+using System;
+using System.IO;
+using Xunit;
+
+namespace P371.ASDML.Tests
+{
+    public class StressTests
+    {
+        const int TRESHOLD = 20;
+
+        [Fact]
+        public void CreateData()
+        {
+            Func<string> random = () => "ASDML_" + Path.GetRandomFileName().Replace(".", "");
+            using (var asdml = new StreamWriter($"../../../data{TRESHOLD}.asdml"))
+            {
+                for (int i = 0; i < TRESHOLD; i++)
+                {
+                    asdml.WriteLine($"{random()} {{");
+                    for (int j = 0; j < TRESHOLD; j++)
+                    {
+                        asdml.WriteLine($"  .{random()} {random()}");
+                    }
+                    for (int j = 0; j < TRESHOLD; j++)
+                    {
+                        asdml.WriteLine($"  {random()} {{");
+                        for (int k = 0; k < TRESHOLD; k++)
+                        {
+                            asdml.WriteLine($"    .{random()} {random()}");
+                        }
+                        for (int k = 0; k < TRESHOLD; k++)
+                        {
+                            asdml.WriteLine($"    {random()} {{");
+                            for (int l = 0; l < TRESHOLD; l++)
+                            {
+                                asdml.WriteLine($"      .{random()} {random()}");
+                            }
+                            asdml.WriteLine($"    }}");
+                        }
+                        asdml.WriteLine($"  }}");
+                    }
+                    asdml.WriteLine($"}}");
+                }
+            }
+        }
+
+        [Fact]
+        public void StressTest1()
+        {
+            Parser parser = new Parser(new FileInfo($"../../../data{TRESHOLD}.asdml"));
+            var output = parser.Parse();
+        }
+    }
+}
